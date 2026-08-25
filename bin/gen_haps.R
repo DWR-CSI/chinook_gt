d84ea92ec3deb196337a8f6234efdabfc2fad83e
Bryan <9120001+Talitrus@users.noreply.github.com>
2026-08-25 11:03:40 -0700

    no longer removing trailing _R123 in sample names

--- bin/gen_haps.R
+++ bin/gen_haps.R
@@ -21,7 +21,6 @@ clean_sample_name <- function(x) {
   x %>%
     str_remove("_$") %>% # Remove trailing underscore
     str_remove("_S\\d+_?$") %>% # Remove _S123_ suffix
-    str_remove("_R\\d+_?$") %>% # Remove _R123_ suffix
     str_remove("_L\\d+_?$") # Remove _L123_ suffix
 }
 
